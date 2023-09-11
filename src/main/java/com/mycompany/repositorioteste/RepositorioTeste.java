d7330b6ac68f99b1959006716ae79dc12d7169ce
ROMARIO DOS SANTOS <romario.2919@aluno.pr.senac.br>
2023-09-11 11:12:10 -0300

    primeiro comit

---
+++ src/main/java/com/mycompany/repositorioteste/RepositorioTeste.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.repositorioteste;
+
+/**
+ *
+ * @author romario.2919
+ */
+public class RepositorioTeste {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
